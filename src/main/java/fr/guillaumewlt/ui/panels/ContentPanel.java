d1533cc042a52213908c3b3e1f95df22ef916b0f
Guillaume <walterguillaume53@gmail.com>
2026-04-04 14:57:21 +0200

    modified content panel for UI Structuration

---
+++ src/main/java/fr/guillaumewlt/ui/panels/ContentPanel.java
@@ -0,0 +1,11 @@
+package fr.guillaumewlt.ui.panels;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ContentPanel extends JPanel {
+
+    public ContentPanel() {
+        setLayout(new BorderLayout());
+    }
+}
